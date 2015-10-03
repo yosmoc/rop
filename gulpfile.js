6e44221b31fbc0cd650946a5c72fc7158ec268eb
samurai20000 <samurai20000@gmail.com>
2015-10-03 23:41:25 +0200

    Use gulp for compiling js / html / css file

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var babel = require('gulp-babel');
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var watch = require('gulp-watch');
+
+gulp.task('compile',
+    [
+        'compile-es6',
+        'compile-html',
+        'compile-css'
+    ]
+);
+
+gulp.task('compile-es6', function() {
+    return gulp.src('src/**/*.js')
+        .pipe(plumber())
+        .pipe(babel())
+        .pipe(gulp.dest('./app'));
+});
+
+gulp.task('compile-html', function() {
+    return gulp.src('src/**/*.html')
+        .pipe(gulp.dest('./app'));
+});
+
+gulp.task('compile-css', function() {
+    return gulp.src('src/**/*.css')
+        .pipe(gulp.dest('./app'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.js', ['compile']);
+});
