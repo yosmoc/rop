009e182b9fe943e5db91cd8c2ca5b11937458215
samurai20000 <samurai20000@gmail.com>
2015-10-04 02:05:33 +0200

    Add rotatepdf.js

---
+++ src/rotatepdf.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class Rotatepdf {
+  constructor(inputFile, angle) {
+    this.inputFile = inputFile;
+    this.angle = angle;
+  }
+
+  run() {
+    var command = `pdftk ${this.inputFile.path} cat 1-endeast output ${this.inputFile.path}.pdf`
+    console.log(command);
+    var cp = require('child_process');
+    cp.exec(command, {timeout: 1000}, function(error, stdout, stderr) {
+    if(stdout){
+      console.log('stdout: ' + stdout);
+    }
+    if(stderr){
+      console.log('stderr: ' + stderr);
+    }
+    if (error !== null) {
+      console.log('Exec error: ' + error);
+    });
+  }
+}
